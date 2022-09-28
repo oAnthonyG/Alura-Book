eca96436de0be33e866f7f78e141782d0031eb1e
Anthony Guilherme <anthonytguilherme@hotmail.com>
2022-09-28 14:06:10 -0300

    create the error message and finally message

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const consultCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+const consultCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+.then(Response => Response.json()
+.then(r => {
+  if(r.erro){
+    throw Error('Esse CEP não existe')
+  }else  
+  console.log(r)
+})
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento conclúido!')));
 
 console.log(consultCEP)
